f822294c517aa6ea4c48dd717c1e48af66f93e0b
BrokebackDrug <fangtianyu0208@gmail.com>
2022-07-02 00:07:20 +0800

    add server GUI

--- DS_rmi/chatClient/src/com/remote/server/InterfaceServer.java
+++ DS_rmi/chatClient/src/com/remote/server/InterfaceServer.java
@@ -15,24 +15,24 @@ public interface InterfaceServer extends Remote{
     //distribute a shared file to all connected clients
     void broadcastMessage(ArrayList<Integer> inc,List<String> list,String filename) throws RemoteException;
     
-    //cette fonction pour recupere le nom des clients connectes
+    //retrieve the name of the connected clients
     Vector<String> getListClientByName(String name) throws RemoteException;
     
-    //cette fonction pour ajouter un client connectées a la liste des clients connectées
+    //add a connected client to the list of connected clients
     void addClient(InterfaceClient client) throws RemoteException;
     
-    //cette fonction pour blocker un client d"envoyer un message, mais il peut recu les messages
+    // block a client from sending a message, but it can receive the messages
     void blockClient(List<String> clients) throws RemoteException;
     
-    //cette fonction pour supprimer totalement une liste des clients de chat (kick-out)
+    //completely remove a list of chat clients (kick-out)
     void removeClient(List<String> clients) throws RemoteException;
     
-    //cette fonction pour supprimer totalement un seul client de chat (kick-out)
+    //completely remove a single chat client (kick-out)
     void removeClient(String clients) throws RemoteException;
     
-    //cette fonction pour activer un client dans chat, d'apres etre dans le cas de "block"
+    //activate a client in chat, according to be in the case of "block"
     void reactiveClient(List<String> clients) throws RemoteException;
     
-    //cette fonction pour verfifier est que un username existe deja dans le serveur ou non, car username est l'identificateur sur chat
+    //check if a username already exists in the server or not, because username is the identifier on chat
     boolean checkUsername(String username) throws RemoteException;
 }
--- DS_rmi/chatServer/src/com/remote/server/Main.java
+++ DS_rmi/chatServer/src/com/remote/server/Main.java
@@ -1,18 +1,63 @@
 package com.remote.server;
 
+import java.awt.*;
+import javax.swing.*;
+import java.util.Date;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 
-public class Main {
-    public static void main(String []args){
-        try {
-            LocateRegistry.createRegistry(4321);
+
+public class Main extends JFrame {
+    
+    public JTextArea jta = new JTextArea();
+     // constructor
+    public Main()
+    {
+
+    }
+        public static void main (String[] args) {
+        Main main= new Main();
+        main.build();
+    }
+  
+//    public static void main(String []args){
+//        try {
+//            LocateRegistry.createRegistry(4321);
+//            Naming.rebind("rmi://localhost:4321/remote",new ChatServer());
+//            System.out.println("Server started at " + new Date() + '\n'");
+//        } catch (MalformedURLException | RemoteException ex) {
+//            System.out.println("Error: " + ex.getMessage());
+//        }
+//    }
+    
+         public void build(){
+        Main s1 = new Main();
+        try{
+           LocateRegistry.createRegistry(4321);
             Naming.rebind("rmi://localhost:4321/remote",new ChatServer());
-            System.out.println("Server Started ...");
-        } catch (MalformedURLException | RemoteException ex) {
+
+            System.out.println ("Server is ready.");
+            s1.jta.append("Server started at " + new Date() + '\n');
+
+            s1.setLayout(new BorderLayout());
+            s1.add(new JScrollPane(s1.jta), BorderLayout.CENTER);
+            s1.setTitle("Server");
+            s1.setSize(500,600);
+
+            s1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            s1.setVisible(true);
+            s1.jta.setEditable(false);
+        }catch(MalformedURLException | RemoteException ex){
             System.out.println("Error: " + ex.getMessage());
         }
     }
+
+    public void displayMsg(Main obj, String s){
+        obj.jta.append(s + ".\n");
+    }
+        
+    
+    
 }
